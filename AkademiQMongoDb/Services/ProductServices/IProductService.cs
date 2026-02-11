cd1e72610d993b4489701200ea4aac17365d85d6
MnrMine <mnr_mine_agri@hotmail.com>
2026-02-11 21:58:34 +0300

    Services klasör eklendi.

---
+++ AkademiQMongoDb/Services/ProductServices/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using AkademiQMongoDb.Dtos.ProductDto;
+
+namespace AkademiQMongoDb.Services.ProductServices
+{
+    public interface IProductService
+    {
+        Task<List<ResultProductDto>> GetAllProductAsync();
+        Task CreateProductAsync(CreateProductDto productDto);
+        Task UpdateProductAsync(UpdateProductDto productDto);
+        Task DeleteProductAsync(string id);
+        Task<GetProductByIdDto> GetProductByIdAsync(string id);
+
+    }
+}
---
+++ AkademiQMongoDb/Services/TestimonialServices/ITestimonialService.cs
@@ -0,0 +1,13 @@
+﻿using AkademiQMongoDb.Dtos.TestimonialDto;
+
+namespace AkademiQMongoDb.Services.TestimonialServices
+{
+    public interface ITestimonialService
+    {
+        Task<List<ResultTestimonialDto>> GetAllTestimonialAsync();
+        Task CreateTestimonialAsync(CreateTestimonialDto testimonialDto);
+        Task UpdateTestimonialAsync(UpdateTestimonialDto testimonialDto);
+        Task DeleteTestimonialAsync(string id);
+        Task<GetTestimonialByIdDto> GetTestimonialByIdAsync(string id);
+    }
+}
